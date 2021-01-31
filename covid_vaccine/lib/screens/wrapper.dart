0078caeb958ad42100a545d0f6bb6d77f88bf1f1
AdiSuresh <adithyasuresh201@gmail.com>
2021-02-01 00:45:32 +0530

    restructuring & link w/ firebase

---
+++ covid_vaccine/lib/screens/wrapper.dart
@@ -0,0 +1,10 @@
+import 'package:covid_vaccine/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // return home or authenticate widget
+    return Home();
+  }
+}
