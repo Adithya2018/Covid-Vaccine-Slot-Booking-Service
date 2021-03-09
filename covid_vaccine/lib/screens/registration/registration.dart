d092736dc97c49cf1d7b43e1927e58df236876c0
adithya suresh <adithyasuresh201@gmail.com>
2021-03-10 02:23:56 +0530

    UI changes to QR code scanner

--- covid_vaccine/lib/screens/registration/registration.dart
+++ covid_vaccine/lib/screens/registration/registration.dart
@@ -1,5 +1,6 @@
 //import 'dart:html';
 import 'dart:io' show Platform;
+import 'dart:math';
 import 'package:covid_vaccine/services/auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
@@ -851,10 +852,11 @@ class QRCodeScanner extends StatefulWidget {
 }
 
 class _QRCodeScannerState extends State<QRCodeScanner> {
-  bool cameraActive = true;
-
   QRViewController controller;
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  bool cameraActive = true;
+  bool flashOn = false;
+
   void toggleCameraState() {
     setState(
       () {
@@ -891,11 +893,9 @@ class _QRCodeScannerState extends State<QRCodeScanner> {
     super.dispose();
   }
 
-  Color toggleFlashBtnColor = Colors.black;
-  bool flashOn = false;
-
-  Future<CameraFacing> isFlashOn() async {
-    return await controller?.getCameraInfo();//getFlashStatus()?.then((val) => flashOn = val)?.onError((error, stackTrace) => null);
+  Future<bool> isFlashOn() async {
+    return await controller
+        ?.getFlashStatus(); //getFlashStatus()?.then((val) => flashOn = val)?.onError((error, stackTrace) => null);
   }
 
   @override
@@ -923,7 +923,11 @@ class _QRCodeScannerState extends State<QRCodeScanner> {
                           children: [
                             IconButton(
                               iconSize: 45,
-                              splashColor: Colors.white,
+                              highlightColor: Colors.transparent,
+                              splashColor: flashOn
+                                  ? Colors.transparent
+                                  : Colors.yellow[200], //Color(0xFFFFF176),
+                              splashRadius: MediaQuery.of(context).size.width,
                               onPressed: () async {
                                 await controller?.toggleFlash()?.whenComplete(
                                   () {
@@ -935,9 +939,8 @@ class _QRCodeScannerState extends State<QRCodeScanner> {
                               },
                               icon: Icon(
                                 Icons.flash_on,
-                                color: flashOn
-                                    ? Color(0xFFFFF176)
-                                    : Colors.black,
+                                color:
+                                    flashOn ? Colors.yellow[200] : Colors.black,
                               ),
                             ),
                             FutureBuilder(
@@ -958,13 +961,19 @@ class _QRCodeScannerState extends State<QRCodeScanner> {
                         iconSize: 75,
                         onPressed: () async {
                           if (cameraActive) {
-                            await controller
-                                ?.pauseCamera()
-                                ?.whenComplete(() => toggleCameraState());
+                            await controller?.pauseCamera()?.whenComplete(() {
+                              setState(() {
+                                cameraActive = false;
+                              });
+                            });
                           } else {
                             await controller?.resumeCamera()?.whenComplete(
-                                  () => toggleCameraState(),
-                                );
+                              () {
+                                setState(() {
+                                  cameraActive = true;
+                                });
+                              },
+                            );
                           }
                         },
                         icon: cameraActive
@@ -1020,11 +1029,23 @@ class _QRCodeScannerState extends State<QRCodeScanner> {
   void _onQRViewCreated(QRViewController controller) {
     setState(() {
       this.controller = controller;
+      this.controller?.resumeCamera()?.whenComplete(() {
+        cameraActive = true;
+      })?.catchError((e){
+        cameraActive = true;
+        print(e.toString());
+      });
+      this.controller?.getFlashStatus()?.then((val) {
+        flashOn = val;
+      })?.catchError((e){
+        flashOn = false;
+        print(e.toString());
+      });
     });
     controller.scannedDataStream.listen((val) async {
       await controller?.stopCamera()?.whenComplete(() {
         widget.toggleView(val);
-      });
+      })?.catchError((e) => print(e.toString()));
     });
   }
 }
